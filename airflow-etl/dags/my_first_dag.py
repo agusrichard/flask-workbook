2d7286b39d797aef576b9a207f9bb41cc013ea21
Agus Richard <agus.richard21@gmail.com>
2022-06-09 00:21:10 +0700

    My first dag

--- airflow-etl/dags/my_first_dag.py
+++ airflow-etl/dags/my_first_dag.py
@@ -1,4 +1,31 @@
+from datetime import timedelta, datetime
 from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
 
-with DAG() as dag:
-    pass
+default_args = {
+    "owner": "airflow",
+    "retries": 3,
+    "retry_delay": timedelta(minutes=5),
+}
+
+args = {
+    "dag_id": "my_first_dag",
+    "description": "This is my first DAG",
+    "start_date": datetime.now(),
+    "schedule_interval": "@daily",
+    "default_args": default_args,
+}
+
+with DAG(**args) as dag:
+    task1 = BashOperator(
+        task_id="task1", bash_command="echo 'Hello World! This is task one'"
+    )
+    task2 = BashOperator(
+        task_id="task2", bash_command="echo 'Hello World! This is task two'"
+    )
+
+    task3 = BashOperator(
+        task_id="task3", bash_command="echo 'Hello World! This is task three'"
+    )
+
+    task1 >> [task2, task3]
